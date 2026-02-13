ac1bfef753a51e73e330dba2d680b5e601a3cdc7
praveen-047 <praveen636317@gmail.com>
2026-02-13 13:37:48 +0530

    view students added

--- src/pages/AddStudent.jsx
+++ src/pages/AddStudent.jsx
@@ -1,21 +1,37 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from "axios"
+import { useNavigate } from 'react-router-dom';
 const AddStudent = () => {
   const[name,setName] = useState("")
   const[email,setEmail] = useState("")
   const[depertment,setdpt] = useState("")
   const[course,setCourse] = useState("")
+
+  const navigate = useNavigate()
+  
   
   function handleForm(e){
     e.preventDefault()
     let user = {name,email,depertment,course}
+    console.log("handle form function");
+    
+    axios.post("http://localhost:5000/users",user)
+    .then(()=>{
+      setName("")
+      setEmail("")
+      setdpt("")
+      setCourse("")
+      navigate("/viewstudent")
+    })
+    .catch(err=>console.log(err));
+    
   }
   
   return (
     <>
       <center>Add Student</center>
       <center>
-        <form>
+        <form action="post" onSubmit={handleForm}>
           <input type="text" placeholder='enter name' value={name}  onChange={(e)=>setName(e.target.value)}/>
           <br />
           <input type="text" placeholder='enter email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
@@ -23,6 +39,8 @@ const AddStudent = () => {
           <input type="text" placeholder='enter depertment' value={depertment} onChange={(e)=>setdpt(e.target.value)}/>
           <br />
           <input type="text" placeholder='enter course' value={course} onChange={(e)=>setCourse(e.target.value)}/>
+          <br />
+          <button type='submit'>Add Student</button>
         </form>
       </center>
     </>
--- src/pages/ViewStudent.jsx
+++ src/pages/ViewStudent.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
-
+import axios from 'axios';
+import { useState,useEffect } from 'react';
 const ViewStudent = () => {
+  const [users,setUser] = useState([])
+
+  useEffect(()=>{
+    fetchData();
+  },[users])
+
+  function fetchData(){
+    axios.get("http://localhost:5000/users")
+    .then((x)=>setUser(x.data))
+    .catch((err)=>console.log(err))
+  }
   return (
-    <div>ViewStudent</div>
+    <div className='view-container'>
+      <h1>Students Record</h1>
+      {users.map((each)=>(
+        <div className='view-card'>
+          <p>name:{each.name}</p>
+          <p>email:{each.email}</p>
+          <p>depertment:{each.depertment}</p>
+          <p>course:{each.course}</p>
+          <button className='edit'>Edit</button>
+          <button className='delete'>Delete</button>
+        </div>
+      ))}
+    </div>
   );
 };
 
